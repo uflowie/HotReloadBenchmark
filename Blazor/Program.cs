d17c64d77f083f308545186f24bfbd405e31e01a
Florin Ulrich <florin.ulrich@hdi.at>
2025-04-17 18:59:59 +0200

    add radzen

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazor;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
